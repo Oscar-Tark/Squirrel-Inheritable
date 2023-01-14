5e92da3a7b1eb9307269876fd9fb5a40bae5e9d8
Ferret <oscartark91@protonmail.com>
2023-01-14 18:05:48 +0100

    Added Disposable class

---
+++ SquirrelDisposable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SquirrelInheritable
+{
+    public class SquirrelDisposable : IDisposable
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    
+        internal bool Disposed { get; private set; }
+    
+        protected virtual void Dispose(bool disposing)
+        {
+            Disposed = true;
+        }
+    }
+}
